b7bb2be73fd786b94c7f51a79c51ec5fa7590288
anrachana28-del <anrachana28@gmail.com>
2026-05-26 12:16:29 +0700

    Create server.js

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.use(express.static("public"));
+
+io.on("connection", (socket) => {
+  console.log("User connected:", socket.id);
+
+  socket.on("join", (room) => {
+    socket.join(room);
+  });
+
+  socket.on("signal", (data) => {
+    io.to(data.room).emit("signal", data.message);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected:", socket.id);
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+  console.log("Server running on port", PORT);
+});
