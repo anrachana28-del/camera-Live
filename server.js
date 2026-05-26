a8ae564963fc36d5f77fcc86219619f09999a25f
anrachana28-del <anrachana28@gmail.com>
2026-05-26 12:44:21 +0700

    Update server.js

--- server.js
+++ server.js
@@ -10,17 +10,18 @@ app.use(express.static("public"));
 
 io.on("connection", (socket) => {
 
-  socket.on("join", (room) => {
-    socket.join(room);
+  socket.on("join-camera", (cameraId) => {
+    socket.join(cameraId);
   });
 
   socket.on("signal", (data) => {
-    io.to(data.room).emit("signal", data.message);
+    io.to(data.cameraId).emit("signal", data);
   });
 
 });
 
 const PORT = process.env.PORT || 3000;
+
 server.listen(PORT, () => {
-  console.log("Server running on port", PORT);
+  console.log("Server running:", PORT);
 });
